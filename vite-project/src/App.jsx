bd3546eb59dff23893a7a2979349a767a860e9d7
tatanoi <ta.phasin@gmail.com>
2025-03-25 17:10:01 +0700

    pre-deploy

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,8 @@
 // App.js
 import React, { useState } from 'react';
 import './App.css';
+// Access-Control-Allow-Credentials: true
+// Access-Control-Allow-Origin: http://127.0.0.1:5000
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
